87e03fbaaa2847c327d69e44a7b86d07eb10fb7e
basta <jsembasta@gmail.com>
2024-11-01 23:39:53 +0100

    Some initial db for requesting schedule

--- src/frontend/web/src/Routes.tsx
+++ src/frontend/web/src/Routes.tsx
@@ -7,11 +7,37 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route } from '@redwoodjs/router'
+import { Set, Router, Route } from '@redwoodjs/router'
+
+import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 
 const Routes = () => {
   return (
     <Router>
+      <Set wrap={ScaffoldLayout} title="Users" titleTo="users" buttonLabel="New User" buttonTo="newUser">
+        <Route path="/users/new" page={UserNewUserPage} name="newUser" />
+        <Route path="/users/{id:Int}/edit" page={UserEditUserPage} name="editUser" />
+        <Route path="/users/{id:Int}" page={UserUserPage} name="user" />
+        <Route path="/users" page={UserUsersPage} name="users" />
+      </Set>
+      <Set wrap={ScaffoldLayout} title="UserSchedules" titleTo="userSchedules" buttonLabel="New UserSchedule" buttonTo="newUserSchedule">
+        <Route path="/user-schedules/new" page={UserScheduleNewUserSchedulePage} name="newUserSchedule" />
+        <Route path="/user-schedules/{id:Int}/edit" page={UserScheduleEditUserSchedulePage} name="editUserSchedule" />
+        <Route path="/user-schedules/{id:Int}" page={UserScheduleUserSchedulePage} name="userSchedule" />
+        <Route path="/user-schedules" page={UserScheduleUserSchedulesPage} name="userSchedules" />
+      </Set>
+      <Set wrap={ScaffoldLayout} title="Users" titleTo="users" buttonLabel="New User" buttonTo="newUser">
+      </Set>
+      <Set wrap={ScaffoldLayout} title="Users" titleTo="users" buttonLabel="New User" buttonTo="newUser">
+      </Set>
+      <Set wrap={ScaffoldLayout} title="UserScheduleDays" titleTo="userScheduleDays" buttonLabel="New UserScheduleDay" buttonTo="newUserScheduleDay">
+      </Set>
+      <Set wrap={ScaffoldLayout} title="Users" titleTo="users" buttonLabel="New User" buttonTo="newUser">
+      </Set>
+      <Set wrap={ScaffoldLayout} title="UserSchedules" titleTo="userSchedules" buttonLabel="New UserSchedule" buttonTo="newUserSchedule">
+      </Set>
+      <Set wrap={ScaffoldLayout} title="UserSchedules" titleTo="userSchedules" buttonLabel="New UserSchedule" buttonTo="newUserSchedule">
+      </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
