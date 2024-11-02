47d213c3d6e06896e78f35761b4b400c916900f1
Vikvam <vikvam@disroot.org>
2024-11-03 00:10:58 +0100

    Seed script for users fix

--- src/frontend/scripts/seed.ts
+++ src/frontend/scripts/seed.ts
@@ -37,7 +37,7 @@ export default async () => {
         const rtg_preference = Math.random()
         return {
           name: `RTGAsistent${idx + 1}`,
-          occupation: 'nurse',
+          occupation: 'rtgAssistant',
           rtg_preference: rtg_preference,
           ct_preference: 1 - rtg_preference,
         }
